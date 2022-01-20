b1e48566f73fc293de3574f404339db602c6aa90
naenkko <nastya_babenko2005@mail.ru>
2022-01-20 22:41:19 +1000

    добавлена картинка для сердца

--- heart.py
+++ heart.py
@@ -6,10 +6,10 @@ class Heart(pygame.sprite.Sprite):
     def __init__(self, x_coord, y_coord, width, height, group):
         super().__init__()
 
-        # self.image = pygame.image.load('images/?.png')
+        self.image = pygame.image.load('images/heart.png')
 
-        self.image = pygame.Surface([width, height])
-        self.image.fill((255, 0, 119))
+        # self.image = pygame.Surface([width, height])
+        # self.image.fill((255, 0, 119))
 
         self.rect = self.image.get_rect()
         self.rect.x = x_coord
